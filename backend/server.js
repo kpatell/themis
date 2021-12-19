a240471f27147d59cf5e604ea685b868dc5e7034
krishanpatel <krishanpatel00@gmail.com>
2021-12-19 12:24:11 -0500

    add cors functionality

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 // create express app
 const app = express();
 
+app.use(cors());
+
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Configuring the database
 const dbConfig = require('./config/database.config.js');
@@ -18,6 +21,7 @@ mongoose.Promise = global.Promise;
 
 // Connecting to the database
 mongoose.connect(dbConfig.url, {
+    useUnifiedTopology: true,
     useNewUrlParser: true
 }).then(() => {
     console.log("Successfully connected to the database");    
@@ -26,15 +30,18 @@ mongoose.connect(dbConfig.url, {
     process.exit();
 });
 
-// define a simple route
-app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Themis. Virtual Queuing made easy."});
+// get default connection
+var db = mongoose.connection;
+
+// define a simple route to test
+app.get('/test', (req, res) => {
+    res.json({"message": "If you're seeing this, test was successful :)"});
 });
 
 // Require queue routes
 require('./app/routes/element.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(3001, () => {
+    console.log("Server is listening on port 3001");
 });
